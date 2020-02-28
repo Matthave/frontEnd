158d88554bbb3ff1445d6537a98a7ce3ae435367
Matt <matthave@gmail.com>
2020-02-29 00:34:23 +0100

    Updates

---
+++ precache-manifest.6f6258eb67cd446dde004718ea2c9f61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e084a932cbd57331992fab47c1afb2a5",
+    "url": "/frontEnd/index.html"
+  },
+  {
+    "revision": "2b417a678c4d28f5901b",
+    "url": "/frontEnd/static/css/main.1be4f7fe.chunk.css"
+  },
+  {
+    "revision": "f271969c7352decb59fa",
+    "url": "/frontEnd/static/js/2.61f844b0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/frontEnd/static/js/2.61f844b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b417a678c4d28f5901b",
+    "url": "/frontEnd/static/js/main.fa36c122.chunk.js"
+  },
+  {
+    "revision": "9ee290165be41d8ecd07",
+    "url": "/frontEnd/static/js/runtime-main.466070ab.js"
+  }
+]);

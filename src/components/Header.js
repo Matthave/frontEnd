be70dc509cc31986f56c761a56c0a3cc1d78aaae
Matt <matthave@gmail.com>
2020-03-20 18:57:29 +0100

    Add arrow in header components, style fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,54 @@
 import React from 'react'
 import Cube from '../pages/Cube'
 
-function Header(props) {
-  const classes = ['introLogo'];
-  if (props.introLogoMoveUp) classes.push('introLogo--onTop')
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+    this.scroll = this.scroll.bind(this)
+  }
+  state = {
+    arrowDown: false,
+  }
 
-  return (
-    <section className="header">
-      <Cube />
-    </section>
-  )
+  componentDidMount() {
+    document.addEventListener('scroll', this.scroll)
+    setTimeout(() => {
+      this.setState({
+        arrowDown: true,
+      })
+    }, 2500);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('scroll', this.scroll)
+  }
+
+  scroll = () => {
+    const scrollHeight = window.scrollY;
+    if (scrollHeight === 0) {
+      setTimeout(() => {
+        this.setState({
+          arrowDown: true,
+        })
+      }, 1000);
+    } else if (scrollHeight > 0) {
+      this.setState({
+        arrowDown: false,
+      })
+    }
+  }
+
+  render() {
+    const classes = ['introLogo'];
+    if (this.props.introLogoMoveUp) classes.push('introLogo--onTop')
+    return (
+      <section className="header">
+        <Cube />
+        {this.state.arrowDown ? <div className="arrowDown"><i className="fas fa-chevron-down"></i></div> : null}
+
+      </section>
+    )
+  }
 }
 
 export default Header

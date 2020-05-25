0c02fd2f85b168d8dedfc59958eaff4a42fb542a
Matt <matthave@gmail.com>
2020-05-25 22:17:20 +0200

    Updates

---
+++ precache-manifest.59215db4b8b8c420ea6d8864b953288f.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "776038594f8888b47748e8f57e18572b",
+    "url": "/frontEnd/index.html"
+  },
+  {
+    "revision": "ca90c32d2ffe8723dee1",
+    "url": "/frontEnd/static/css/main.a8d2f382.chunk.css"
+  },
+  {
+    "revision": "e4e0bc2c0131876ceb80",
+    "url": "/frontEnd/static/js/2.05f449c6.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/frontEnd/static/js/2.05f449c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca90c32d2ffe8723dee1",
+    "url": "/frontEnd/static/js/main.c6ced3da.chunk.js"
+  },
+  {
+    "revision": "9ee290165be41d8ecd07",
+    "url": "/frontEnd/static/js/runtime-main.466070ab.js"
+  },
+  {
+    "revision": "cdfe47b31e9184a55cf02eef1baf7240",
+    "url": "/frontEnd/static/media/Azonix.cdfe47b3.otf"
+  },
+  {
+    "revision": "d7d067771b01cdd88d0bc7125359e787",
+    "url": "/frontEnd/static/media/NewLogoSmallTransparent.d7d06777.png"
+  },
+  {
+    "revision": "83b9ebfa9dcc7ddb3ff8390683f3f5a4",
+    "url": "/frontEnd/static/media/christmas.83b9ebfa.png"
+  },
+  {
+    "revision": "a42b5954bf016db6d3498191a80ac222",
+    "url": "/frontEnd/static/media/cvOnline.a42b5954.png"
+  },
+  {
+    "revision": "71a41f0ee4a4d888d05e68b25be1ab8d",
+    "url": "/frontEnd/static/media/cvOnline2.71a41f0e.png"
+  },
+  {
+    "revision": "5da7ec2e806bffa4bcaaf18984fba452",
+    "url": "/frontEnd/static/media/game.5da7ec2e.png"
+  },
+  {
+    "revision": "c62aded729bf7146d491275e5019d7fc",
+    "url": "/frontEnd/static/media/gilroy-light.c62aded7.otf"
+  },
+  {
+    "revision": "0dfbca95659c0dd3c7b41351442465ba",
+    "url": "/frontEnd/static/media/headerSlider.0dfbca95.png"
+  },
+  {
+    "revision": "c48ff48b3b9ab747e5ed2ae96d09f2eb",
+    "url": "/frontEnd/static/media/kyokushinSite.c48ff48b.png"
+  },
+  {
+    "revision": "667a8c456153b0d365f26d4afca4df71",
+    "url": "/frontEnd/static/media/toDoApp.667a8c45.png"
+  },
+  {
+    "revision": "bd2f7495da0d0a157669be4b8142e82c",
+    "url": "/frontEnd/static/media/trainerSite.bd2f7495.png"
+  },
+  {
+    "revision": "5d395ab46751a4bd7e661d6b133696c6",
+    "url": "/frontEnd/static/media/urlShorten.5d395ab4.png"
+  },
+  {
+    "revision": "97430b1f0c7da4c57893a7c0578d3bd4",
+    "url": "/frontEnd/static/media/virus192.97430b1f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5087cb3cd972fe78b6cf274abe6fd082.js"
+  "/frontEnd/precache-manifest.59215db4b8b8c420ea6d8864b953288f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/frontEnd/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
